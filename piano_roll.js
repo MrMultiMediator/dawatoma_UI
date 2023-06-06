7d1c10e2edc342c894e0de17d6c6f440aaf28894
MrMultiMediator <mrmultimediator@gmail.com>
2023-06-06 07:10:53 -0600

    Added piano_roll.js for development of piano roll. Updated notes/notes.txt with TODO items for note addition.

---
+++ piano_roll.js
@@ -0,0 +1,66 @@
+const piano = document.getElementsByClassName("piano_roll"); // Get all piano_roll elements
+const everything = document.getElementsByTagName("div"); // Get all div elements
+
+piano_elements = piano[0].children
+const note_elements = document.getElementsByClassName("note");
+console.log(note_elements.length)
+
+/*
+for (let i = 0; i < piano_elements.length; i++) { 
+    console.log(piano_elements[i].className != "g107_256")
+    console.log(piano_elements[i].className != "g107_255")
+}
+
+/*console.log(everything.length);
+console.log(piano.length);
+console.log(piano[0]);*/
+
+//piano[0].classList.add("note");
+// Add a note to the piano (eventually we want this to be triggered when a user clicks
+// on the piano roll)
+piano[0].insertAdjacentHTML('beforeend','<div id="note'+'1'+'" class="note"></div>')
+
+// Create variable that stores the note element we just created
+let qqq = document.getElementById("note1")
+console.log(qqq)
+qqq.note_num = 1
+
+console.log("TOP = ",parseInt(getComputedStyle(qqq).top))
+
+// One way to change the location of a note
+if (false) {qqq.style.top = (qqq.offsetTop + 16) + "px";}
+
+// Another way to change the top location of a note
+if (false) {qqq.style.top = (parseInt(getComputedStyle(qqq).top, 10) + 16) + "px";}
+
+// Another way to change the location of a note
+//qqq.style.top = "33px";
+console.log("TOP = ",parseInt(getComputedStyle(qqq).top))
+
+//piano[0].removeChild(qqq)
+
+qqq.held = false // If the note is being held
+
+qqq.addEventListener('mousedown', mousedown);
+qqq.addEventListener('mouseup', mouseup);
+
+/*
+function mousedown(e){
+    qqq.held = true
+    console.log("held = ",qqq.held)
+    qqq.style.backgroundColor="#00FFFF";
+}
+
+function mouseup(e){
+    qqq.style.backgroundColor="green";
+    qqq.held = false
+    console.log("held = ",qqq.held)
+    qqq.style.qq = 10 + "px";
+    console.log("top = ",getComputedStyle(qqq).getPropertyValue('top'))
+    console.log("left = ",getComputedStyle(qqq).getPropertyValue('left'))
+    console.log("height = ",getComputedStyle(qqq).height)
+    console.log("gg = ",getComputedStyle(qqq).getPropertyValue('--row_num'))
+    qqq.style.setProperty('--row_num', parseInt(getComputedStyle(qqq).getPropertyValue('--row_num'))+1);
+    qqq.style.setProperty('--col_num', parseInt(getComputedStyle(qqq).getPropertyValue('--col_num'))+1);
+    console.log("gg = ",getComputedStyle(qqq).getPropertyValue('--row_num'))
+} */
